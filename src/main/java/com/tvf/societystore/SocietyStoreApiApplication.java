767b9923a9743fbf8fe61e3e2ca9f5364734353f
surajbadhe <surajdbadhe@gmail.com>
2025-08-16 17:05:00 +0100

    First Commit, Added folder structure, required dependencies

---
+++ src/main/java/com/tvf/societystore/SocietyStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.tvf.societystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietyStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tvf/societystore/SocietyStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvf.societystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
